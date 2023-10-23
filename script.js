5ce91e67260b73d6b4c6e340298e33c21222a921
agus-mb <munosagustina3@gmail.com>
2023-10-23 18:34:27 -0300

    info de la API plasmada en el html

--- script.js
+++ script.js
@@ -5,10 +5,29 @@ const fetchData =position=>{
     const {latitude, longitude}=position.coords;//tomamos del objeto su atributo position: la latirud y la longitud
     fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)//pedimos la data a la API
     .then(response=>response.json())//mandamos la data traducida a json para mayor legibilidad
-    .then(data=>console.log(data))
+    .then(data=>setWeatherData(data))
 
 console.log(position);//mis coordenadas
 }
+
+const setWeatherData=data=>{
+    console.log(data)//keys de la api
+    const weatherData={
+        location: data.name,
+        description: data.weather[0].main,
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        temperatura: data.main.temp,
+        date:'data',
+    }//ingresamos a la api y evocamos de acuerdo a la ubicacion de las keys
+
+    Object.keys(weatherData).forEach(key=>{
+        document.getElementById(key).textContent=weatherData[key];
+    })
+}
+
+
+
 const onLoad=()=>{
     navigator.geolocation.getCurrentPosition(fetchData) //cuando cargue el navegador tomara la geo localiacion guardada en la variable log
 }
