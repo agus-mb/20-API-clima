30b45e3eb09b7033aeb3929379ad6581fa81c7be
agus-mb <munosagustina3@gmail.com>
2023-10-23 21:58:11 -0300

    finish

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const fetchData = (position) => {
   fetch(
     `http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
   ) //pedimos la data a la API
-    .then(response => response.json()) //mandamos la data traducida a json para mayor legibilidad
-    .then(data => setWeatherData(data));
+    .then((response) => response.json()) //mandamos la data traducida a json para mayor legibilidad
+    .then((data) => setWeatherData(data));
 
   console.log(position); //mis coordenadas
 };
@@ -26,20 +26,22 @@ const setWeatherData = (data) => {
     document.getElementById(key).textContent = weatherData[key];
   });
 
-  cleanUp()
+  cleanUp();
 };
 
-const cleanUp=()=>{
-    let container =document.getElementById('container');
-    let loader= document.getElementById('loader');
+const cleanUp = () => {
+  let container = document.getElementById("container");
+  let loader = document.getElementById("loader");
 
-    loader.style.display='none';
-    container.style.display='flex';
-}
+  loader.style.display = "none";
+  container.style.display = "flex";
+};
 
 const getDate = () => {
   let date = new Date();
-  return `${date.getDate()}-${('0' + (date.getMonth()+1)).slice(-2)}-${date.getFullYear()}`; //fecha, slice para
+  return `${date.getDate()}-${("0" + (date.getMonth() + 1)).slice(
+    -2
+  )}-${date.getFullYear()}`; //fecha, slice para
 };
 
 const onLoad = () => {
